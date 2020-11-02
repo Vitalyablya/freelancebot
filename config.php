ae17dbfca1c0694e35be2fd533e8f68111dc039d
Vitalyablya <65458245+Vitalyablya@users.noreply.github.com>
2020-11-02 19:18:51 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-define("AUTO_TOKEN", "764a290139c17aaed388b17ff123115391bf8451");
-define("BOT_TOKEN", "1407696883:AAHIdieRnxc5EO-94ftdlbz26p6Y1MnKB4k");
-define("CHAT_ID", "1065391848");
+define("AUTO_TOKEN", "");
+define("BOT_TOKEN", "");
+define("CHAT_ID", "");
 
 echo "<pre>";
 ?>
